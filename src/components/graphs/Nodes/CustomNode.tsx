4399a6988f25f69725f2352228bcc30095a9d338
Paul49070 <paul.lemonnier49070@gmail.com>
2024-03-27 15:49:50 +0100

    Draggable avec ctrl et tirer les edges depuis le node direct

--- src/components/graphs/Nodes/CustomNode.tsx
+++ src/components/graphs/Nodes/CustomNode.tsx
@@ -1,24 +1,49 @@
 import { FC } from "react";
-import { Handle, NodeProps, Position } from "reactflow";
+import { Handle, NodeProps, Position, useKeyPress } from "reactflow";
 import React from 'react'
 import { nodeStyle } from "../../../styles/Graphes/NodeStyle";
+import {useStore } from 'reactflow';
+import "./CustomNodeStyle.css"
 import theme from "../../../constantes/Colors";
 
 interface CustomNodeProps extends NodeProps {
-  data: { label: string };
+  data: { label: string },
 }
 
+const connectionNodeIdSelector = (state: any) => state.connectionNodeId;
+
 export const CustomNode: FC<CustomNodeProps> = ({ data, selected }) => {
+  const connectionNodeId = useStore(connectionNodeIdSelector);
+  const ctrlKeyPressed = useKeyPress("Control")
+
+  const isConnecting = !!connectionNodeId;
 
   const node_style = nodeStyle(selected)
-  
   return (
     <>
-      <Handle type="target" position={Position.Top} />
-        <div style={node_style}>
-            <p style={{color: theme.light.NodeFont}}>{data.label}</p>
-        </div>
-      <Handle type="source" position={Position.Bottom} id="a" />
+      <div style={node_style}>
+        {!ctrlKeyPressed && !selected ?
+            <>
+              {!isConnecting &&
+                <Handle className="customHandle" position={Position.Bottom} type="source" />
+              }
+
+              <Handle
+                className="customHandle"
+                position={Position.Left}
+                type="target"
+                isConnectableStart={false}
+              />
+            </>
+
+            :
+
+            undefined
+
+        }
+
+        <p style={{color: selected ? theme.light.NodeFont : theme.light.Font}}>{data.label}</p>
+      </div>
     </>
   );
 }
