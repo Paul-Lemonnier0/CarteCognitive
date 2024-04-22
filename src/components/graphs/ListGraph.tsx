6883bbaddb27b1ccf0c45ce5cd623653149cd3f5
Kyldof <kyllian.ronne@laposte.net>
2024-04-22 11:16:18 +0200

    ajout d'un boutton refresh

--- src/components/graphs/ListGraph.tsx
+++ src/components/graphs/ListGraph.tsx
@@ -1,6 +1,10 @@
-import React, { CSSProperties } from "react"
+import React, { CSSProperties, useContext } from "react"
 import { GraphType } from "../../types/Graph/GraphType"
 import GraphPresentation from "./GraphPresentation"
+import { IconButton } from "../Buttons/IconButtons"
+import { FiRefreshCcw } from "react-icons/fi";
+import { getUserGraphs } from "../../firebase/FireStore.tsx/FirestoreDB";
+import { AppContext } from "../../context/AppContext";
 
 
 interface ListGraphInterface {
@@ -10,11 +14,23 @@ interface ListGraphInterface {
 }
 
 const ListGraph = ({graphs, title, style} : ListGraphInterface) => {
+    const {user, setGraphsUser} =useContext(AppContext)
+
     return (
     <div style={style}
         className="graphPresentationListContainer">
         <h2>{title}</h2>
-        <div className="ListGraph">
+        <div className="ListGraph" style={{position : "relative"}}>
+            <div style={{
+                position : "absolute",
+                right : 10,
+                top: 10
+            }}>
+            <IconButton Icon={FiRefreshCcw} onPress={async () =>{
+                const graphCollection = await getUserGraphs(user.uid);
+                setGraphsUser(graphCollection);
+            }}/>
+            </div>
             {
                 graphs.map(graph => (
                     <GraphPresentation graph={graph} style={{
