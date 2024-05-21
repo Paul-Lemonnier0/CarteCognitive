7dc2792662faa7b62f1fe75182a0512fad2b994d
Nicolas-Frnr <nicolas.frnr15@gmail.com>
2024-05-21 17:28:57 +0200

    creation graphe auto

--- src/components/SideBar/AutomatingSideBar.tsx
+++ src/components/SideBar/AutomatingSideBar.tsx
@@ -7,7 +7,11 @@ import { MdLabel } from "react-icons/md";
 import { ValidationButton } from "../Buttons/Buttons";
 import { AppContext } from "../../context/AppContext";
 import { GraphContext } from "../../context/GraphContext";
-
+import { createNewNodeObject } from "../../primitives/NodesMethods";
+import { TypesNode } from "../../context/GraphContext";
+import { SizeType } from "../../context/GraphContext";
+import { PositionType } from "../../context/AppContext";
+import { Edge } from "reactflow";
 
 interface AutomatingSideBarProps {
     isExpanded: boolean
@@ -16,9 +20,12 @@ interface AutomatingSideBarProps {
 
 const AutomatingSideBar: FC<AutomatingSideBarProps> = ({isExpanded}) => {
 
-    const {graphCalculType, edges, setEdges} = useContext(GraphContext)
+    const {graphCalculType, edges, setEdges, nodes, setNodes,addNewEdge} = useContext(GraphContext)
     const [borneMin, setBorneMin] = useState(0)
     const [borneMax, setBorneMax] = useState(1)
+    const [nbrNode, setNbrNode] = useState(1)
+    const [density, setDensity] = useState(0)
+    const [generation, setGeneration] = useState(0)
     const clickAutoComplete = () => {
     
         if(graphCalculType === "Integer"){
@@ -71,6 +78,49 @@ const AutomatingSideBar: FC<AutomatingSideBarProps> = ({isExpanded}) => {
         setBorneMax(parseFloat(event.target.value))
     }
 
+    const changeNbrNode = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setNbrNode(parseInt(event.target.value))
+    }
+    const changeNbrDensity = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if(parseInt(event.target.value) <= 100) setDensity(parseInt(event.target.value))
+    }
+
+    const clickAutoCreate = () => {
+        let x = 0
+        let y = 0
+        const currentNodes = nodes
+        for(let i = 0; i < nbrNode; i++) {
+            if(i%5 === 0) {
+                x = 0
+                y += 500
+            }
+            const node = createNewNodeObject(`${i}${generation}`, (i+1).toString(), { x, y }, TypesNode.customNode, Date.now().toString())
+            x += 500
+            currentNodes.push(node)
+            
+        }  
+        setNodes([...currentNodes])  
+        
+        const currentEdges = edges
+        const nbrEdge = Math.round(nbrNode/100*density)
+        for(let i = 0; i < nbrNode; i++) {
+            for(let y = 0; y <= nbrEdge; y++) {
+                const next = Math.floor(Math.random() * (nbrNode))
+                if(next!=i ) {
+                    const id = "edge_" + i.toString() + generation.toString() + "-" + y.toString() + generation.toString()
+                    currentEdges.push({source:`${i}${generation}`, target:`${next}${generation}`,id:id,data:{label:""}} as Edge)
+                    
+                }
+            }
+            
+            
+        }
+        setEdges([...currentEdges])
+        
+        
+        setGeneration(generation+1)
+    }
+
 
     if (!isExpanded) return null
     return (
@@ -84,7 +134,7 @@ const AutomatingSideBar: FC<AutomatingSideBarProps> = ({isExpanded}) => {
                 <div className="autoComplete">
                     {
                         graphCalculType === "Integer" ? 
-                        <div className="">
+                        <>
                             <div className="borne">
                                 <input className="inputBorne" type="number" max={borneMax-1} onChange={changeMin} value={borneMin}/>
                                 <MidText bold text="Borne Inferieur"/>
@@ -93,10 +143,21 @@ const AutomatingSideBar: FC<AutomatingSideBarProps> = ({isExpanded}) => {
                                 <input className="inputBorne" type="number" min={borneMin+1} onChange={changeMax} value={borneMax}/>
                                 <MidText bold text="Borne Superieur"/>
                             </div>
-                        </div>
+                        </>
                             : undefined
                     }
                     <ValidationButton text="Completer les arretes non définis" onPress={clickAutoComplete}/>
+                </div>
+                <div className="autoCreate">
+                    <div className="parametreAutoCreate">
+                        <input className="inputCreate" type="number" min={1} onChange={changeNbrNode} value={nbrNode}/>
+                        <MidText bold text="nombre Noeud(s)"/>
+                    </div>
+                    <div className="parametreAutoCreate">
+                        <input className="inputCreate" type="number" min={0} max={100} onChange={changeNbrDensity} value={density}/>
+                        <MidText bold text="Densitée (%)"/>
+                    </div>
+                    <ValidationButton text="Créer des nouveaux noeuds" onPress={clickAutoCreate}/>
                 </div>
             </div>
          </div>
